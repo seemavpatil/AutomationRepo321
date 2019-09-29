a0a7195dceae47c2185b5387fdb8316ddea0faf7
seema v patil <seemavpatil1990@gmail.com>
2019-09-29 11:21:38 +0530

    this is our java code

---
+++ BasicCodeJava/src/DatTtypesAndVariables/FirstJavaClass.java
@@ -0,0 +1,12 @@
+package DatTtypesAndVariables;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Selenium");
+
+	}
+       
+       
+}
